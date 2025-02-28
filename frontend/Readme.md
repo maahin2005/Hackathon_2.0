a5ff402a6ba2c5bc50c20558844ce2d6141c30b5
maahin2005 <mahin.malek2005@gmail.com>
2025-03-01 00:38:03 +0530

    Initialize the Project

---
+++ frontend/Readme.md
@@ -0,0 +1,8 @@
+# Activity Tracker Frontend
+
+## Tech Stack:
+
+1. Vite React App
+2. Tailwind CSS
+3. Google font - Ubuntu and Kanit
+4. Redux Toolkit
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./components/Home/Home";
+
+function App() {
+  return (
+    <>
+      <div className="font-[Ubuntu]">
+        <Home />
+      </div>
+    </>
+  );
+}
+
+export default App;
