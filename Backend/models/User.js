47bcfc9bb9b24e60cb6e96433ca7c4f5542ccdc0
maahin2005 <mahin.malek2005@gmail.com>
2025-03-02 13:44:30 +0530

    Github and Google controllers and token in cookies

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,10 +1,22 @@
 // server/models/User.js
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  googleId: String,
-  name: String,
-  email: String,
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    profileImage: { type: String, required: true },
+    googleId: { type: String, default: null },
+    githubId: { type: String, default: null },
+    githubUsername: { type: String },
+    score: { type: Number, default: 0 },
+    role: {
+      type: String,
+      enum: ["jobseeker", "recruiter"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("User", userSchema);
